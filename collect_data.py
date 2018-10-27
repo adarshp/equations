560bc3df30b3178612fbdaaae079482493c0d708
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2018-10-27 13:24:59 -0700

    collect data

--- collect_data.py
+++ collect_data.py
@@ -17,29 +17,32 @@ from latex import tokenize, extract_equations
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('texfile')
+    parser.add_argument('--outdir', default='output')
     parser.add_argument('--template', default='misc/template.tex')
     args = parser.parse_args()
     return args
 
 
 
-def render_tex(filename, outdir='output'):
+def render_tex(filename, outdir):
     """render latex document"""
     dirname = os.path.dirname(filename)
     basename = os.path.basename(filename)
     command = ['latexmk', '-outdir=' + outdir, '-pdf', basename]
     returncode = subprocess.call(command, cwd=dirname)
-    pdf_name = os.path.join(dirname, outdir, os.path.splitext(basename)[0] + '.pdf')
+    pdf_name = os.path.join(outdir, os.path.splitext(basename)[0] + '.pdf')
     return pdf_name
 
 
 
 def render_equation(equation, template, filename):
-    tex = ''.join(repr(c) for c in equation)
-    equation_tex = template.render(equation=tex)
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    equation_tex = template.render(equation=equation)
     with open(filename, 'w') as f:
         f.write(equation_tex)
-    pdf_name = render_tex(filename)
+    pdf_name = render_tex(filename, dirname)
     image = get_pages(pdf_name)[0]
     return image
 
@@ -74,21 +77,45 @@ def match_template(pages, template):
 
 if __name__ == '__main__':
     args = parse_args()
+    paper_id = os.path.basename(os.path.dirname(args.texfile))
+    outdir = os.path.abspath(os.path.join(args.outdir, paper_id))
+    # read latex tokens from document
     tokens = tokenize(args.texfile)
+    # extract equations from token stream
     equations = extract_equations(tokens)
-    pdf_name = render_tex(args.texfile)
+    # compile pdf from document
+    pdf_name = render_tex(args.texfile, outdir)
+    # retrieve pdf pages as images
     pages = get_pages(pdf_name)
+    # load jinja2 template
     template_loader = jinja2.FileSystemLoader(searchpath='.')
     template_env = jinja2.Environment(loader=template_loader)
     template = template_env.get_template(args.template)
-    for j, eq_tex in enumerate(equations):
-        equation = render_equation(eq_tex, template, 'examples/equation.tex')
-        cv2.imwrite('template.jpeg', equation)
+    for j, eq_toks in enumerate(equations, start=1):
+        eq_tex = ''.join(repr(c) for c in eq_toks)
+        eq_name = 'equation-%03d' % j
+        # make pdf
+        fname = os.path.join(outdir, eq_name, 'equation.tex')
+        equation = render_equation(eq_tex, template, fname)
+        # write tex code
+        fname = os.path.join(outdir, eq_name, 'equation-tex.txt')
+        with open(fname, 'w') as f:
+            print(eq_tex, file=f)
+        # find page and aabb where equation appears
         match, i, start, end = match_template(pages, equation)
-        print('=' * 70)
-        print(eq_tex)
-        print('=' * 70)
+        # write image with aabb
         image = pages[i].copy()
         image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
         cv2.rectangle(image, start, end, (0, 0, 255), 2)
-        cv2.imwrite('eq%s.jpeg' % j, image)
+        img_name = os.path.join(outdir, eq_name, 'aabb.png')
+        cv2.imwrite(img_name, image)
+        # write aabb to file (using relative coordinates)
+        fname = os.path.join(outdir, eq_name, 'aabb.tsv')
+        h, w = image.shape[:2]
+        x1 = start[0] / w
+        y1 = start[1] / h
+        x2 = end[0] / w
+        y2 = end[1] / h
+        with open(fname, 'w') as f:
+            print('page\tx1\ty1\tx2\ty2', file=f)
+            print('%s\t%s\t%s\t%s\t%s' % (i, x1, y1, x2, y2), file=f)
